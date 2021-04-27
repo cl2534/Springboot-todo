b7fcede85979b5d26e0b9d780497650e42a9701d
github username <chang.lim92@gmail.com>
2021-04-27 19:21:52 +0900

    Controller update

--- src/main/java/org/example/controller/TodoController.java
+++ src/main/java/org/example/controller/TodoController.java
@@ -1,9 +1,12 @@
 package org.example.controller;
 
 import lombok.AllArgsConstructor;
+import org.example.model.TodoEntity;
+import org.example.model.TodoRequest;
 import org.example.model.TodoResponse;
 import org.example.service.TodoService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.ws.Response;
@@ -17,12 +20,22 @@ public class TodoController {
     private final TodoService service;
 
     @PostMapping
-    public ResponseEntity<TodoResponse> create() {
+    public ResponseEntity<TodoResponse> create(@RequestBody TodoRequest request) {
         System.out.println("CREATE");
-        return null;
+        if (ObjectUtils.isEmpty(request.getTitle())) {
+            return ResponseEntity.badRequest().build();
+        }
+        if (ObjectUtils.isEmpty(request.getOrder())) {
+            request.setOrder(0L);
+        }
+        if (ObjectUtils.isEmpty(request.getCompleted())) {
+            request.setCompleted(false);
+        }
+        TodoEntity result = this.service.add(request);
+        return ResponseEntity.ok(new TodoResponse(result));
     }
 
-    @GetMapping
+    @GetMapping("{id}")
     public ResponseEntity<TodoResponse> readOne() {
         System.out.println("READ ONE");
         return null;
