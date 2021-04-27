57fef30897eb8e7b74b16ba25ab12e2513c0cf1e
github username <chang.lim92@gmail.com>
2021-04-27 21:17:12 +0900

    test error

---
+++ src/test/java/org/example/service/TodoServiceTest.java
@@ -0,0 +1,40 @@
+package org.example.service;
+
+import org.example.model.TodoEntity;
+import org.example.model.TodoRequest;
+import org.example.repository.TodoRepository;
+import org.hibernate.service.spi.InjectService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TodoServiceTest {
+
+    @Mock
+    private TodoRepository todoRepository;
+
+    @InjectMocks
+    private TodoService todoService;
+
+    @Test
+    void add() {
+        when(this.todoRepository.save(any(TodoEntity.class)))
+                .then(AdditionalAnswers.returnsFirstArg());
+        TodoRequest expected = new TodoRequest();
+        expected.setTitle("Test Title");
+        TodoEntity actual = this.todoService.add(expected);
+        assertEquals(expected.getTitle(), actual.getTitle());
+    }
+
+    @Test
+    void searchById() {
+    }
+}
