e7377b9c99792c64a0868c4197949e05769a0fa3
github username <chang.lim92@gmail.com>
2021-04-29 20:25:49 +0900

    adapter pattern end

--- src/main/java/org/example/adapter/Main.java
+++ src/main/java/org/example/adapter/Main.java
@@ -2,9 +2,21 @@ package org.example.adapter;
 
 public class Main {
     public static void main(String [] args) {
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V adapterConditioner = new SocketAdapter(airConditioner);
+        connect(adapterConditioner);
+
+
 
     }
-    public void connect(Electronic220V) {
-
+    public static void connect(Electronic110V electronic110V) {
+        electronic110V.powerOn();
     }
 }
---
+++ src/main/java/org/example/adapter/SocketAdapter.java
@@ -0,0 +1,12 @@
+package org.example.adapter;
+
+public class SocketAdapter implements Electronic110V{
+    private Electronic220V electronic220V;
+    public SocketAdapter(Electronic220V electronic220V) {
+        this.electronic220V = electronic220V;
+    }
+    public void powerOn() {
+        electronic220V.connect();
+    }
+
+}
